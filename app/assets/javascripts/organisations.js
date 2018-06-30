0ef84af01b5196603cff8208da1e5029aaf8f21a
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2018-06-30 17:25:51 +0100

    Replace placeholder with dummy organisation names

--- app/assets/javascripts/organisations.js
+++ app/assets/javascripts/organisations.js
@@ -1,17 +1,12 @@
 $(document).ready(function () {
-  var states = ['alabama', 'alaska', 'arizona', 'Arkansas', 'California',
-    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+  var organisations = [
+    {id: 'dummyOrg1', name: "Dummy org 1"},
+    {id: 'dummyOrg2', name: "Dummy org 2"},
+    {id: 'dummyOrg3', name: "Dummy org 3"}
   ];
 
   $('.organisation-name').typeahead({
-    source: states,
+    source: organisations,
     autoSelect: true
   });
 });
