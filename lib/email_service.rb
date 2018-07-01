aa9c4899fb6cd6b32a2e04e70a3c72bc0cbfe991
Suzanne Hamilton <suzanne.m.hamilton@gmail.com>
2018-07-01 14:43:59 +0100

    Revert "Send an example image in emails"
    This reverts commit 2739a0a5831455cc21bb42d5758e488333412665.

--- lib/email_service.rb
+++ lib/email_service.rb
@@ -1,5 +1,4 @@
 require "sendgrid-ruby"
-require "base64"
 
 class EmailService
   include SendGrid
@@ -7,17 +6,9 @@ class EmailService
   def send(to_email, from_email, subject, body)
     to = Email.new(email:to_email)
     from = Email.new(email: from_email)
-    
-    image = Base64.strict_encode64(File.read("config/example_id.jpg"))
-    image_content = SendGrid::Attachment.new
-    image_content.type = "jpeg"
-    image_content.content = image
-    image_content.filename = "id.jpg"
 
-    content = SendGrid::Content.new(type: "text/html", value: body)
+    content = SendGrid::Content.new(type: "text/plain", value: body)
     mail    = SendGrid::Mail.new(from, subject, to, content)
-    mail.add_attachment(image_content)
-
     sg      = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
 
     response = sg.client.mail._('send').post(request_body: mail.to_json)
